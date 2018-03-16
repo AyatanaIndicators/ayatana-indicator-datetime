67a3944dad4efda6ab20c4b6d6b1d96e245ba8c3
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2018-03-16 20:51:44 +0000

    Geoclue -> Geoclue2 blind shot. More namespace changes.

--- src/timezone-geoclue.cpp
+++ src/timezone-geoclue.cpp
@@ -25,7 +25,7 @@
 #include <string>
 #include <vector>
 
-#define GEOCLUE_BUS_NAME "org.freedesktop.Geoclue.Master"
+#define GEOCLUE_BUS_NAME "org.freedesktop.Geoclue2.Master"
 
 namespace ayatana {
 namespace indicator {
