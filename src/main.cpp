fdd18c9d53ca50b435c5965f748b2c32a745e8a7
Nicholas Guriev <nicholas@guriev.su>
2021-04-24 23:10:17 +0300

    Various tests fixes
     * Provide default EdsEngine constructor for autotests.
     * Temporary disable DST related tests. Something strange happens there.
     * Do not create locale datetime for more reliable result on non-UTC systems.
     * Specify audio_url for sample Alarms.
    
     Fixes AyatanaIndicators/ayatana-indicator-datetime#32

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,6 @@
 #include <datetime/exporter.h>
 #include <datetime/locations-settings.h>
 #include <datetime/menu.h>
-#include <datetime/myself.h>
 #include <datetime/planner-aggregate.h>
 #include <datetime/planner-snooze.h>
 #include <datetime/planner-range.h>
@@ -61,7 +60,7 @@ namespace
         if (!g_strcmp0("lightdm", g_get_user_name()))
             engine.reset(new MockEngine);
         else
-            engine.reset(new EdsEngine(std::shared_ptr<Myself>(new Myself)));
+            engine.reset(new EdsEngine);
 
         return engine;
     }
