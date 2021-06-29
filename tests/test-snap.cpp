ubports/indicator-datetime

re-enable sound tests in test-snap

tests/test-snap.cpp
@@ -641,7 +641,7 @@ std::string path_to_uri(const std::string& path)
   return uri;
 }
 
-TEST_F(SnapFixture,DISABLED_DefaultSounds)
+TEST_F(SnapFixture,DefaultSounds)
 {
   auto settings = std::make_shared<Settings>();
   auto ne = std::make_shared<uin::Engine>(APP_NAME);

AyatanaIndicators/ayatana-indicator-datetime

tests/test-snap.cpp
@@ -548,7 +548,7 @@ TEST_F(SnapFixture, ForceScreen)
 ****
 ***/
 
-TEST_F(SnapFixture,DISABLED_Vibrate)
+TEST_F(SnapFixture,Vibrate)
 {
   auto settings = std::make_shared<Settings>();
   auto ne = std::make_shared<ayatana::indicator::notifications::Engine>(APP_NAME);