ubports/indicator-datetime

Update unit test.

tests/notification-fixture.h
@@ -53,7 +53,7 @@ protected:
   static constexpr char const * HAPTIC_METHOD_VIBRATE_PATTERN {"VibratePattern"};
 
   static constexpr int SCREEN_COOKIE {8675309};
-  static constexpr char const * SCREEN_METHOD_KEEP_DISPLAY_ON {"keepDisplayOn"}; 
+  static constexpr char const * SCREEN_METHOD_KEEP_DISPLAY_ON {"keepDisplayOn"};
   static constexpr char const * SCREEN_METHOD_REMOVE_DISPLAY_ON_REQUEST {"removeDisplayOnRequest"};
 
   static constexpr int POWERD_SYS_STATE_ACTIVE = 1;
@@ -111,7 +111,7 @@ protected:
     const auto christmas = unity::indicator::datetime::DateTime::Local(2015,12,25,0,0,0);
     appt.begin = christmas.start_of_day();
     appt.end = christmas.end_of_day();
-    appt.alarms.push_back(unity::indicator::datetime::Alarm{"Ho Ho Ho!", "", appt.begin});
+    appt.alarms.push_back(unity::indicator::datetime::Alarm{"Ho Ho Ho!", "", appt.begin, unity::indicator::datetime::Alarm::SOUND });
 
     // init an Ubuntu Alarm
     ualarm.color = "red";
@@ -165,8 +165,8 @@ protected:
                                                 NOTIFY_INTERFACE,
                                                 &error);
     g_assert_no_error(error);
-   
-    // METHOD_GET_INFO 
+
+    // METHOD_GET_INFO
     str = g_strdup("ret = ('mock-notify', 'test vendor', '1.0', '1.1')");
     dbus_test_dbus_mock_object_add_method(notify_mock,
                                           notify_obj,
@@ -196,7 +196,7 @@ protected:
     g_assert_no_error (error);
     g_free (str);
 
-    // METHOD_CLOSE 
+    // METHOD_CLOSE
     str = g_strdup_printf("self.EmitSignal('%s', '%s', 'uu', [ args[0], %d ])",
                           NOTIFY_INTERFACE,
                           SIGNAL_CLOSED,
@@ -223,8 +223,8 @@ protected:
                                                 BUS_POWERD_INTERFACE,
                                                 &error);
     g_assert_no_error(error);
-   
-    str = g_strdup_printf ("ret = '%s'", POWERD_COOKIE); 
+
+    str = g_strdup_printf ("ret = '%s'", POWERD_COOKIE);
     dbus_test_dbus_mock_object_add_method(powerd_mock,
                                           powerd_obj,
                                           POWERD_METHOD_REQUEST_SYS_STATE,
@@ -256,8 +256,8 @@ protected:
                                                 BUS_SCREEN_INTERFACE,
                                                 &error);
     g_assert_no_error(error);
-   
-    str = g_strdup_printf ("ret = %d", SCREEN_COOKIE); 
+
+    str = g_strdup_printf ("ret = %d", SCREEN_COOKIE);
     dbus_test_dbus_mock_object_add_method(screen_mock,
                                           screen_obj,
                                           SCREEN_METHOD_KEEP_DISPLAY_ON,
@@ -287,7 +287,7 @@ protected:
                                                 BUS_HAPTIC_PATH,
                                                 BUS_HAPTIC_INTERFACE,
                                                 &error);
-   
+
     dbus_test_dbus_mock_object_add_method(haptic_mock,
                                           haptic_obj,
                                           HAPTIC_METHOD_VIBRATE_PATTERN,

AyatanaIndicators/ayatana-indicator-datetime

tests/notification-fixture.h
@@ -53,7 +53,7 @@ protected:
   static constexpr char const * HAPTIC_METHOD_VIBRATE_PATTERN {"VibratePattern"};
 
   static constexpr int SCREEN_COOKIE {8675309};
-  static constexpr char const * SCREEN_METHOD_KEEP_DISPLAY_ON {"keepDisplayOn"}; 
+  static constexpr char const * SCREEN_METHOD_KEEP_DISPLAY_ON {"keepDisplayOn"};
   static constexpr char const * SCREEN_METHOD_REMOVE_DISPLAY_ON_REQUEST {"removeDisplayOnRequest"};
 
   static constexpr int POWERD_SYS_STATE_ACTIVE = 1;
@@ -111,7 +111,7 @@ protected:
     const auto christmas = ayatana::indicator::datetime::DateTime::Local(2015,12,25,0,0,0);
     appt.begin = christmas.start_of_day();
     appt.end = christmas.end_of_day();
-    appt.alarms.push_back(ayatana::indicator::datetime::Alarm{"Ho Ho Ho!", "", appt.begin});
+    appt.alarms.push_back(ayatana::indicator::datetime::Alarm{"Ho Ho Ho!", "", appt.begin, ayatana::indicator::datetime::Alarm::SOUND });
 
     // init a Lomiri Alarm
     ualarm.color = "red";
@@ -165,8 +165,8 @@ protected:
                                                 NOTIFY_INTERFACE,
                                                 &error);
     g_assert_no_error(error);
-   
-    // METHOD_GET_INFO 
+
+    // METHOD_GET_INFO
     str = g_strdup("ret = ('mock-notify', 'test vendor', '1.0', '1.1')");
     dbus_test_dbus_mock_object_add_method(notify_mock,
                                           notify_obj,
@@ -196,7 +196,7 @@ protected:
     g_assert_no_error (error);
     g_free (str);
 
-    // METHOD_CLOSE 
+    // METHOD_CLOSE
     str = g_strdup_printf("self.EmitSignal('%s', '%s', 'uu', [ args[0], %d ])",
                           NOTIFY_INTERFACE,
                           SIGNAL_CLOSED,
@@ -223,8 +223,8 @@ protected:
                                                 BUS_POWERD_INTERFACE,
                                                 &error);
     g_assert_no_error(error);
-   
-    str = g_strdup_printf ("ret = '%s'", POWERD_COOKIE); 
+
+    str = g_strdup_printf ("ret = '%s'", POWERD_COOKIE);
     dbus_test_dbus_mock_object_add_method(powerd_mock,
                                           powerd_obj,
                                           POWERD_METHOD_REQUEST_SYS_STATE,
@@ -256,8 +256,8 @@ protected:
                                                 BUS_SCREEN_INTERFACE,
                                                 &error);
     g_assert_no_error(error);
-   
-    str = g_strdup_printf ("ret = %d", SCREEN_COOKIE); 
+
+    str = g_strdup_printf ("ret = %d", SCREEN_COOKIE);
     dbus_test_dbus_mock_object_add_method(screen_mock,
                                           screen_obj,
                                           SCREEN_METHOD_KEEP_DISPLAY_ON,
@@ -287,7 +287,7 @@ protected:
                                                 BUS_HAPTIC_PATH,
                                                 BUS_HAPTIC_INTERFACE,
                                                 &error);
-   
+
     dbus_test_dbus_mock_object_add_method(haptic_mock,
                                           haptic_obj,
                                           HAPTIC_METHOD_VIBRATE_PATTERN,