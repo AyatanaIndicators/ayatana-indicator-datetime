ubports/indicator-datetime

Use an explicit registry object so that it gets free'd when the function exits

src/notifications.cpp
@@ -25,6 +25,7 @@
 #include <messaging-menu/messaging-menu-message.h>
 
 #include <libubuntu-app-launch-2/ubuntu-app-launch/appid.h>
+#include <libubuntu-app-launch-2/ubuntu-app-launch/registry.h>
 
 #include <uuid/uuid.h>
 
@@ -460,7 +461,8 @@ private:
 
     static std::string calendar_app_id()
     {
-        auto app_id = ubuntu::app_launch::AppID::discover("com.ubuntu.calendar");
+        auto registry = std::make_shared<ubuntu::app_launch::Registry>();
+        auto app_id = ubuntu::app_launch::AppID::discover(registry, "com.ubuntu.calendar");
         if (!app_id.empty())
             // Due the use of old API by messaging_menu we need append a extra ".desktop" to the app_id.
             return std::string(app_id) + ".desktop";

AyatanaIndicators/ayatana-indicator-datetime

src/notifications.cpp
@@ -26,6 +26,7 @@
 
 #ifdef HAS_LOMIRIAPPLAUNCH
 #include <liblomiri-app-launch/lomiri-app-launch/appid.h>
+#include <liblomiri-app-launch/lomiri-app-launch/registry.h>
 #endif
 
 #include <uuid/uuid.h>
@@ -463,7 +464,8 @@ private:
     static std::string calendar_app_id()
     {
         #ifdef HAS_LOMIRIAPPLAUNCH
-        auto app_id = lomiri::app_launch::AppID::discover("com.lomiri.calendar");
+        auto registry = std::make_shared<lomiri::app_launch::Registry>();
+        auto app_id = lomiri::app_launch::AppID::discover(registry, "com.lomiri.calendar");
 
         if (!app_id.empty())
             // Due the use of old API by messaging_menu we need append a extra ".desktop" to the app_id.