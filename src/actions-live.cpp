ubports/indicator-datetime

Fix clock app URL.

src/actions-live.cpp
@@ -95,7 +95,7 @@ void LiveActions::open_alarm_app()
 {
     switch(get_desktop()) {
         case LiveActions::UNITY8:
-            dispatch_url("appid://ubuntu-clock-app/ubuntu-clock-app/current-user-version");
+            dispatch_url("alarm://");
             break;
         case LiveActions::UNITY7:
         default:

AyatanaIndicators/ayatana-indicator-datetime

src/actions-live.cpp
@@ -50,7 +50,7 @@ void LiveActions::open_alarm_app()
 {
     if (ayatana_common_utils_is_lomiri())
     {
-        ayatana_common_utils_open_url("appid://lomiri-clock-app/lomiri-clock-app/current-user-version");
+        ayatana_common_utils_open_url("alarm://");
     }
     else
     {