ubports/indicator-datetime

in LiveSettings, add gschema support for notification app blacklisting

include/datetime/settings-shared.h
@@ -52,4 +52,7 @@ TimeFormatMode;
 #define SETTINGS_ALARM_HAPTIC_S         "alarm-haptic-feedback"
 #define SETTINGS_SNOOZE_DURATION_S      "snooze-duration-minutes"
 
+#define SETTINGS_CUNH_SCHEMA_ID         "com.ubuntu.notifications.hub"
+#define SETTINGS_CUNH_BLACKLIST_S       "blacklist"
+
 #endif // INDICATOR_DATETIME_SETTINGS_SHARED

AyatanaIndicators/ayatana-indicator-datetime

include/datetime/settings-shared.h
@@ -51,4 +51,7 @@ TimeFormatMode;
 #define SETTINGS_ALARM_HAPTIC_S         "alarm-haptic-feedback"
 #define SETTINGS_SNOOZE_DURATION_S      "snooze-duration-minutes"
 
+#define SETTINGS_CUNH_SCHEMA_ID         "com.lomiri.notifications.hub"
+#define SETTINGS_CUNH_BLACKLIST_S       "blacklist"
+
 #endif // INDICATOR_DATETIME_SETTINGS_SHARED