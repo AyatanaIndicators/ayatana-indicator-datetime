ubports/indicator-datetime

Only wake device if bubbles notifications are enabled

src/snap.cpp
@@ -97,7 +97,10 @@ public:
         const bool interactive = appointment.is_ubuntu_alarm() && m_engine->supports_actions();
 
         // force the system to stay awake
-        std::shared_ptr<uin::Awake> awake = std::make_shared<uin::Awake>(m_engine->app_name());
+        std::shared_ptr<uin::Awake> awake;
+	if (appointment.is_ubuntu_alarm() || calendar_bubbles_enabled()) {
+	    awake = std::make_shared<uin::Awake>(m_engine->app_name());
+	}
 
         // calendar events are muted in silent mode; alarm clocks never are
         std::shared_ptr<uin::Sound> sound;

AyatanaIndicators/ayatana-indicator-datetime

src/snap.cpp
@@ -96,7 +96,10 @@ public:
         const bool interactive = appointment.is_ubuntu_alarm() && m_engine->supports_actions();
 
         // force the system to stay awake
-        std::shared_ptr<ain::Awake> awake = std::make_shared<ain::Awake>(m_engine->app_name());
+        std::shared_ptr<ain::Awake> awake;
+   if (appointment.is_ubuntu_alarm() || calendar_bubbles_enabled()) {
+       awake = std::make_shared<ain::Awake>(m_engine->app_name());
+   }
 
         // calendar events are muted in silent mode; alarm clocks never are
         std::shared_ptr<ain::Sound> sound;