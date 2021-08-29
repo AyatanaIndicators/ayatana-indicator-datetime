ubports/indicator-datetime

Vibrate only once when notification about calendar events.

src/snap.cpp
@@ -116,7 +116,7 @@ public:
         if (should_vibrate()) {
             const auto haptic_mode = m_settings->alarm_haptic.get();
             if (haptic_mode == "pulse")
-                haptic = std::make_shared<uin::Haptic>(uin::Haptic::MODE_PULSE);
+                haptic = std::make_shared<uin::Haptic>(uin::Haptic::MODE_PULSE, appointment.is_ubuntu_alarm());
         }
 
         // show a notification...

AyatanaIndicators/ayatana-indicator-datetime

src/snap.cpp
@@ -117,7 +117,7 @@ public:
         if (should_vibrate()) {
             const auto haptic_mode = m_settings->alarm_haptic.get();
             if (haptic_mode == "pulse")
-                haptic = std::make_shared<ain::Haptic>(ain::Haptic::MODE_PULSE);
+                haptic = std::make_shared<ain::Haptic>(ain::Haptic::MODE_PULSE, appointment.is_ubuntu_alarm());
         }
 
         // show a notification...