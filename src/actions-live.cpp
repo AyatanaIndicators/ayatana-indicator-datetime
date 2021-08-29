ubports/indicator-datetime

Update app name usage to match snaps.

src/actions-live.cpp
@@ -95,7 +95,7 @@ void LiveActions::open_alarm_app()
 {
     switch(get_desktop()) {
         case LiveActions::UNITY8:
-            dispatch_url("appid://com.ubuntu.clock/clock/current-user-version");
+            dispatch_url("appid://ubuntu-clock-app/ubuntu-clock-app/current-user-version");
             break;
         case LiveActions::UNITY7:
         default:

src/notifications.cpp
@@ -462,7 +462,7 @@ private:
     static std::string calendar_app_id()
     {
         auto registry = std::make_shared<ubuntu::app_launch::Registry>();
-        auto app_id = ubuntu::app_launch::AppID::discover(registry, "com.ubuntu.calendar");
+        auto app_id = ubuntu::app_launch::AppID::discover(registry, "ubuntu-calendar-app");
         if (!app_id.empty())
             // Due the use of old API by messaging_menu we need append a extra ".desktop" to the app_id.
             return std::string(app_id) + ".desktop";

tests/test-live-actions.cpp
@@ -161,7 +161,7 @@ TEST_F(TestLiveActionsFixture, DesktopOpenSettingsApp)
 
 namespace
 {
-    const std::string clock_app_url = "appid://com.ubuntu.clock/clock/current-user-version";
+    const std::string clock_app_url = "appid://ubuntu-clock-app/ubuntu-clock-app/current-user-version";
 }
 
 TEST_F(TestLiveActionsFixture, PhoneOpenAlarmApp)

AyatanaIndicators/ayatana-indicator-datetime

src/actions-live.cpp
@@ -50,7 +50,7 @@ void LiveActions::open_alarm_app()
 {
     if (ayatana_common_utils_is_lomiri())
     {
-        ayatana_common_utils_open_url("appid://com.lomiri.clock/clock/current-user-version");
+        ayatana_common_utils_open_url("appid://lomiri-clock-app/lomiri-clock-app/current-user-version");
     }
     else
     {

src/notifications.cpp
@@ -465,7 +465,7 @@ private:
     {
         #ifdef HAS_LOMIRIAPPLAUNCH
         auto registry = std::make_shared<lomiri::app_launch::Registry>();
-        auto app_id = lomiri::app_launch::AppID::discover(registry, "com.lomiri.calendar");
+        auto app_id = lomiri::app_launch::AppID::discover(registry, "lomiri-calendar-app");
 
         if (!app_id.empty())
             // Due the use of old API by messaging_menu we need append a extra ".desktop" to the app_id.

tests/test-live-actions.cpp
@@ -144,7 +144,7 @@ TEST_F(TestLiveActionsFixture, DesktopOpenSettingsApp)
 
 namespace
 {
-    const std::string clock_app_url = "appid://com.ubuntu.clock/clock/current-user-version";
+    const std::string clock_app_url = "appid://lomiri-clock-app/lomiri-clock-app/current-user-version";
 }
 
 TEST_F(TestLiveActionsFixture, PhoneOpenAlarmApp)