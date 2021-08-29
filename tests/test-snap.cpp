ubports/indicator-datetime

rather than commenting out a test, use Google Test's DISABLED_ feature to disable it

tests/test-snap.cpp
@@ -590,7 +590,6 @@ TEST_F(SnapFixture,Vibrate)
 ****
 ***/
 
-#if 0
 namespace uin = unity::indicator::notifications;
 
 /**
@@ -633,7 +632,7 @@ std::string path_to_uri(const std::string& path)
   return uri;
 }
 
-TEST_F(SnapFixture,DefaultSounds)
+TEST_F(SnapFixture,DISABLED_DefaultSounds)
 {
   auto settings = std::make_shared<Settings>();
   auto ne = std::make_shared<uin::Engine>(APP_NAME);
@@ -659,4 +658,3 @@ TEST_F(SnapFixture,DefaultSounds)
     EXPECT_EQ(test_case.expected_role, sb->role());
   }
 }
-#endif

AyatanaIndicators/ayatana-indicator-datetime

tests/test-snap.cpp
@@ -534,8 +534,7 @@ TEST_F(SnapFixture, ForceScreen)
 ****
 ***/
 
-#if 0
-TEST_F(SnapFixture,Vibrate)
+TEST_F(SnapFixture,DISABLED_Vibrate)
 {
   auto settings = std::make_shared<Settings>();
   auto ne = std::make_shared<ayatana::indicator::notifications::Engine>(APP_NAME);
@@ -582,4 +581,3 @@ TEST_F(SnapFixture,Vibrate)
     EXPECT_EQ(test_case.expected_vibrate_called, vibrate_called);
   }
 }
-#endif