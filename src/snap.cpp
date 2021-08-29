ubports/indicator-datetime

Fix notifications so it respects if it should or not show bubbles or add to notification list

src/snap.cpp
@@ -97,10 +97,7 @@ public:
         const bool interactive = appointment.is_ubuntu_alarm() && m_engine->supports_actions();
 
         // force the system to stay awake
-        std::shared_ptr<uin::Awake> awake;
-        if (appointment.is_ubuntu_alarm() || calendar_bubbles_enabled()) {
-            awake = std::make_shared<uin::Awake>(m_engine->app_name());
-        }
+        std::shared_ptr<uin::Awake> awake = std::make_shared<uin::Awake>(m_engine->app_name());
 
         // calendar events are muted in silent mode; alarm clocks never are
         std::shared_ptr<uin::Sound> sound;
@@ -180,6 +177,9 @@ public:
                 on_response(appointment, alarm, Snap::Response::ShowApp);
             });
         }
+
+        b.set_show_notification_bubble(appointment.is_ubuntu_alarm() || calendar_bubbles_enabled());
+        b.set_post_to_messaging_menu(appointment.is_ubuntu_alarm() || calendar_list_enabled());
 
         const auto key = m_engine->show(b);
         if (key)

AyatanaIndicators/ayatana-indicator-datetime

src/snap.cpp
@@ -96,10 +96,7 @@ public:
         const bool interactive = appointment.is_ubuntu_alarm() && m_engine->supports_actions();
 
         // force the system to stay awake
-        std::shared_ptr<ain::Awake> awake;
-        if (appointment.is_ubuntu_alarm() || calendar_bubbles_enabled()) {
-            awake = std::make_shared<ain::Awake>(m_engine->app_name());
-        }
+        std::shared_ptr<ain::Awake> awake = std::make_shared<ain::Awake>(m_engine->app_name());
 
         // calendar events are muted in silent mode; alarm clocks never are
         std::shared_ptr<ain::Sound> sound;
@@ -182,6 +179,9 @@ public:
                 on_response(appointment, alarm, Snap::Response::ShowApp);
             });
         }
+
+        b.set_show_notification_bubble(appointment.is_ubuntu_alarm() || calendar_bubbles_enabled());
+        b.set_post_to_messaging_menu(appointment.is_ubuntu_alarm() || calendar_list_enabled());
 
         const auto key = m_engine->show(b);
         if (key)